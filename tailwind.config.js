008e114907570fa6ae075bfd8f37de4bad174794
Cesar Santamaria <cesar.santamaria@icloud.com>
2023-08-11 09:48:15 -0500

    Fix missing npm dependencies.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,12 @@ module.exports = {
   content: [
     './config/*.json',
     './layout/*.liquid',
-    '.assets/*.liquid',
-    '.sections/*.liquid',
-    '.snippets/*.liquid',
-    '.templates/*.liquid',
-    '.templates/*.json',
-    '.templates/customers/*.liquid',
+    './assets/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/*.liquid',
+    './templates/*.json',
+    './templates/customers/*.liquid',
   ],
   theme: {
     extend: {},
